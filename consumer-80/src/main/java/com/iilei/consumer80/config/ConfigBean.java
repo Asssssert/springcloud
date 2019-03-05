32f8cfe856c0ca8b51e82c5236649956d57bc4b4
iilei <670182350@qq.com>
2019-03-05 17:36:05 +0800

    使用其他ribbon规则

--- consumer-80/src/main/java/com/iilei/consumer80/config/ConfigBean.java
+++ consumer-80/src/main/java/com/iilei/consumer80/config/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.iilei.consumer80.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RetryRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +14,11 @@ public class ConfigBean {
     @LoadBalanced //开启ribbon负载均衡，客户端
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule iRule() {
+//        return new RandomRule();//随机策略
+        return new RetryRule();//先轮训，如果有失败的 多失败几次以后跳过这个节点
     }
 }
