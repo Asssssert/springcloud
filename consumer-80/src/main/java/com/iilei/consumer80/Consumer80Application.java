257f07117634686086d1fb2da1c9e6c022d0daf2
iilei <670182350@qq.com>
2019-03-05 18:02:13 +0800

    自定义ribbon规则

--- consumer-80/src/main/java/com/iilei/consumer80/Consumer80Application.java
+++ consumer-80/src/main/java/com/iilei/consumer80/Consumer80Application.java
@@ -1,11 +1,14 @@
 package com.iilei.consumer80;
 
+import com.iilei.ribbon.rule.MyRibbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "PROVIDER-DEPT", configuration = MyRibbon.class)
 public class Consumer80Application {
 
     public static void main(String[] args) {
