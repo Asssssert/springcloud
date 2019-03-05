6ea2cc324b170497eb452da91dc4633d7d504efd
iilei <670182350@qq.com>
2019-03-05 16:44:19 +0800

    ribbon负载均衡多微服务配置

---
+++ provider-8003/src/main/java/com/iilei/provider8003/controller/DeptController.java
@@ -0,0 +1,38 @@
+package com.iilei.provider8003.controller;
+
+import com.iilei.api.entity.Dept;
+import com.iilei.provider8003.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DeptController {
+    @Autowired
+    private DeptService deptService;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @PostMapping(value = "/dept/add")
+    public boolean add(@RequestBody Dept dept) {
+        return deptService.add(dept);
+    }
+
+    @GetMapping(value = "/dept/get/{id}")
+    public Dept get(@PathVariable Long id) {
+        return deptService.get(id);
+    }
+
+    @GetMapping(value = "/dept/list")
+    public List<Dept> list() {
+        return deptService.list();
+    }
+
+    @GetMapping(value = "/dept/discovery")
+    public Object discovery() {
+        return this.discoveryClient;
+    }
+
+}
---
+++ provider-8003/src/main/java/com/iilei/provider8003/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.iilei.provider8003.service;
+
+import com.iilei.api.entity.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
