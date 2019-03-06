ba988b113841ec40383980213d5cf217104d1606
iilei <670182350@qq.com>
2019-03-06 17:00:36 +0800

    初步学习zuul

---
+++ zuul-9527/src/main/java/com/iilei/zuul9527/Zuul9527Application.java
@@ -0,0 +1,18 @@
+package com.iilei.zuul9527;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class Zuul9527Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul9527Application.class, args);
+    }
+
+}
